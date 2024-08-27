dbc8747b4291c20f57e10524cbb7f0a5400c68ec
Robert <robertchan.lee903@gmail.com>
2024-08-28 00:56:56 +0800

    add a permission for VIBRATE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,4 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7")
-
-
-//    implementation("io.github.piepacker:jampadcompose:0.0.1-alpha1")
 }
--- app/src/main/java/com/example/joystick/ui/joystick/haptics/HapticGenerator.kt
+++ app/src/main/java/com/example/joystick/ui/joystick/haptics/HapticGenerator.kt
@@ -1,10 +1,5 @@
 package com.example.joystick.ui.joystick.haptics
 
-//import androidx.compose.runtime.Composable
-
 interface HapticGenerator {
     fun generate(type: HapticEffect)
 }
-
-//@Composable
-//expect fun rememberHapticGenerator(): HapticGenerator
